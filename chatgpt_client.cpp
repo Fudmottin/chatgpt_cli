14931c6d35e4d22f11b4c7756ca1d8115db8d1c1
Fudmottin <fudmottin@gmail.com>
2023-05-03 11:19:43 -0400

    Did some stuff.

--- chatgpt_client.cpp
+++ chatgpt_client.cpp
@@ -5,8 +5,9 @@ using json = nlohmann::json;
 
 ChatGPTClient::ChatGPTClient(const string& api_key, const string& api_base_url)
     : AIClient(api_key, api_base_url),
-      model(supported_models[0]), temperature(0.7),
-      chatgpt_response(), max_tokens(2048) {
+      model(supported_models[1]), temperature(0.7),
+      chatgpt_response(), max_tokens(0), counter(0),
+      presence_penalty(0.0), frequency_penalty(0.0) {
     conversation_history = json::array();
     conversation_history.push_back({{"role", "system"}, {"content", "start chat"}});
 }
@@ -16,6 +17,10 @@ void ChatGPTClient::set_model(const string& model) {this->model = model;}
 void ChatGPTClient::set_temperature(float temp) {temperature = temp;}
 
 void ChatGPTClient::set_max_tokens(int tok) {max_tokens = tok;}
+
+void ChatGPTClient::set_presence_penalty(float penalty) {presence_penalty = penalty;}
+
+void ChatGPTClient::set_frequency_penalty(float penalty) {frequency_penalty = penalty;}
 
 string ChatGPTClient::send_message(const string& message) {
         json request_data = {
@@ -27,8 +32,13 @@ string ChatGPTClient::send_message(const string& message) {
                 }
             })},
             {"temperature", temperature},
-	    {"max_tokens", max_tokens}
     	};
+
+	if (max_tokens != 0) request_data.emplace("max_tokens", max_tokens);
+	if (presence_penalty != 0.0)
+	    request_data.emplace("presence_penalty", presence_penalty);
+	if (frequency_penalty != 0.0)
+	    request_data.emplace("frequency_penalty", frequency_penalty);
 
         json response = send_request(request_data);
         chatgpt_response = extract_response(response);
@@ -73,54 +83,11 @@ json ChatGPTClient::send_request(const json& request_data) {
 
     json response_json = json::parse(response.text);
 
-    // Check if the response was truncated due to reaching the token limit
-    if (response_json["choices"][0]["finish_reason"] != "stop" &&
-        !response_json["choices"][0]["text"].is_null()) {
-        string response_text = response_json["choices"][0]["text"].get<string>();
-        string last_words = response_text.substr(response_text.find_last_of(' ') + 1);
-        json new_message = {{"role", "user"}, {"content", last_words}};
-
-        if (!is_similar_to_last_message(new_message)) {
-            json new_request_data = {
-                {"messages", updated_request_data["messages"]},
-                {"model", model},
-                {"temperature", temperature},
-                {"n", 1},
-                {"max_tokens", max_tokens - response_json["choices"][0]["text"].size()}
-            };
-            new_request_data["messages"].push_back(new_message);
-
-            json new_response_json = send_request(new_request_data);
-
-            response_json["choices"][0]["text"] += new_response_json["choices"][0]["text"];
-            response_json["choices"][0]["finish_reason"] = new_response_json["choices"][0]["finish_reason"];
-        }
-    }
-
     conversation_history.push_back(request_data["messages"].back());
     conversation_history.push_back(response_json["choices"][0]["message"]);
 
     return response_json;
 }
-
-bool ChatGPTClient::is_similar_to_last_message(const json& new_message) {
-    if (conversation_history.empty()) {
-        return false;
-    }
-
-    const string& last_message_content = conversation_history.back()["content"];
-    const string& new_message_content = new_message["content"];
-
-    size_t common_prefix_length = 0;
-    while (common_prefix_length < last_message_content.length() &&
-           common_prefix_length < new_message_content.length() &&
-           last_message_content[common_prefix_length] == new_message_content[common_prefix_length]) {
-        ++common_prefix_length;
-    }
-
-    return common_prefix_length > 0.8 * min(last_message_content.length(), new_message_content.length());
-}
-
 
 string ChatGPTClient::extract_response(const json& response) {
     if (response.contains("choices") && !response["choices"].empty()) {
--- chatgpt_client.h
+++ chatgpt_client.h
@@ -14,6 +14,7 @@ using json = nlohmann::json;
 
 const vector<string> supported_models = {
 	"gpt-3.5-turbo",
+	"gpt-4"
 };
 
 class ChatGPTClient : public AIClient {
@@ -24,16 +25,20 @@ public:
 
     void set_model(const string& model);
     void set_temperature(float temp);
+    void set_presence_penalty(float penalty);
+    void set_frequency_penalty(float penalty);
     void set_max_tokens(int tok);
 
 private:
     json send_request(const json& request_data);
     string extract_response(const json& response);
-    bool is_similar_to_last_message(const json& new_message);
 
     string model;
     string chatgpt_response;
     float temperature;
+    float presence_penalty;
+    float frequency_penalty;
     int max_tokens;
+    int counter;
     json conversation_history;
 };
