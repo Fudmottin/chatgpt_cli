51a77d8c04457c41c1e443d3d92f16fa45c852ab
Fudmottin <fudmottin@gmail.com>
2023-05-11 21:04:07 -0400

    Adjusting defaults for temperature, frequency_penalty, and Presence_penalty

--- openai_client.cpp
+++ openai_client.cpp
@@ -6,8 +6,8 @@ using json = nlohmann::json;
 
 OpenAIClient::OpenAIClient(const string& api_key)
 	: client(), api_key(api_key), api_base_url(string("https://api.openai.com/")),
-          temperature(1.0), max_tokens(0), presence_penalty(0.0),
-          frequency_penalty(0.0), conversation_history() {
+          temperature(0.75), max_tokens(0), presence_penalty(0.6),
+          frequency_penalty(0.6), conversation_history() {
     client.SetHeader(cpr::Header{
         {"Authorization", "Bearer " + api_key},
         {"Content-Type", "application/json"},
